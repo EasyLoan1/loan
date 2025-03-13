aafd16af49ff005571175cb70c56aaf23083b5ec
EasyLoan1 <loaneasy377@gmail.com>
2025-03-13 06:34:13 +0530

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -1,26 +1,30 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$bank_account = $_POST['bank_account'];
-$routing_number = $_POST['routing_number'];
 
-$to = 'loaneasy377@gmail.com'; // Update with your email address
-$subject = 'New Loan Application from Website';
+$mg_api = '98b1b5442c6cbb58e26fb97b63d32f5d-623424ea-7bd66881';
+$mg_domain = 'sandbox62d839fd3ae5472d871d931ae03093ea.mailgun.org';
 
-$body = "
-Name: $name\n
-Email: $email\n
-Bank Account: $bank_account\n
-Routing Number: $routing_number\n
-";
+$to = 'loaneasy377@gmail.com';
+$subject = 'Test Email from Loan Easy';
+$body = 'Hello, this is a test email from Loan Easy.';
 
-$headers = "From: $email\r\n";
-$headers .= "Reply-To: $email\r\n";
+require 'vendor/autoload.php';
 
-if (mail($to, $subject, $body, $headers)) {
-    echo "Thank you for your loan application! We'll get back to you soon.";
+use Mailgun\Mailgun;
+
+$mg = new Mailgun($mg_api);
+$domain = $mg_domain;
+
+$result = $mg->sendMessage($domain, array(
+    'from'    => 'postmaster@' . $mg_domain,
+    'to'      => $to,
+    'subject' => $subject,
+    'text'    => $body
+));
+
+if ($result->http_response_code == 200) {
+    echo 'Email sent successfully!';
 } else {
-    echo "Error sending email. Please try again.";
+    echo 'Error sending email.';
 }
-?>
+
 ?>
